b558245c16ddc5672fa4f1f2755d75e2548fcf90
Hari Shankar Karthik <harishankarkarthik.93@gmail.com>
2024-03-21 18:42:51 +0530

    UPDATED: README.md

--- explore_quadrant.py
+++ explore_quadrant.py
@@ -1,5 +1,6 @@
 from random import randint
-from utils.pirate_movements import moveTo, moveAway
+
+percent_chance = lambda percent_chance: randint(1, 100) <= percent_chance
 
 def explore_main_quadrant(pirate, move_1, move_2):
     pirate_signal = pirate.getSignal().split(" ")
@@ -9,9 +10,7 @@ def explore_main_quadrant(pirate, move_1, move_2):
     was_last_move_1 =  pirate_signal[2] == "T"
     current_move = None
 
-    is_favourable = lambda percent_chance: randint(1, 100) <= percent_chance
-
-    if is_favourable(75):
+    if percent_chance(75):
         current_move = move_1 if was_last_move_1 else move_2
     else:
         was_last_move_1 = not was_last_move_1
@@ -20,12 +19,8 @@ def explore_main_quadrant(pirate, move_1, move_2):
     pirate_signal[2] = "T" if was_last_move_1 else "F"
     pirate.setSignal(" ".join(pirate_signal))
     return current_move
-    
-    # last_move = (last_move + 1) % len(primary_moves)
-    
-    # pirate_signal[2] = str(last_move)
-    # pirate.setSignal(" ".join(pirate_signal))
-    # return current_move
 
-    # sample = lambda collection: collection[randint(0, len(collection) - 1)]
-    # return sample(primary_moves)
+def explore_side_quadrant(pirate, primary_move, secondary_moves):
+    sample = lambda collection: collection[randint(0, len(collection) - 1)]
+
+    return primary_move if percent_chance(75) else sample(secondary_moves)
